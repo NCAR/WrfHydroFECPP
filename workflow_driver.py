d687394d9b40ff93a4f21a7bb7ef33d62c9b74c2
Ryan Cabell <rcabell@ucar.edu>
2021-06-18 15:10:12 -0600

    Add new workflow driver

---
+++ workflow_driver.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import os
+
+import ESMF
+from mpi4py import MPI
+
+from fecpp.app import CoastalPreprocessorApp
+from fecpp.slp import SeaLevelPressure
+
+def main():
+    comm = MPI.COMM_WORLD
+    assert comm.Get_size() == ESMF.pet_count()
+
+    # find input forcings
+    dir_date = os.environ["FORCING_BEGIN_DATE"]
+    if len(dir_date) == 12:
+        # remove minutes
+        dir_date = dir_date[:-2]
+        
+    input_path = Path(os.environ["FORCING_OUTPUT_DIR"]) / dir_date
+    if ESMF.local_pet() == 0:
+        print(f"Starting FECPP in {input_path}")
+    
+    app = CoastalPreprocessorApp(input_path, Path(os.environ["GEOGRID_FILE"]))
+    app.regrid_all_files(output_path_transformer=lambda x:x.with_suffix(".latlon.nc"),
+                         var_filter=SeaLevelPressure,
+                         file_filter="**/*LDASIN_DOMAIN*")
+
+
+if __name__ == '__main__':
+    main()
